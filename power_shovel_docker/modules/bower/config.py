d90952bab975d363e4dff1332bf9fb6390e15366
Peter Krenesky <pkrenesky@counsyl.com>
2018-04-16 20:58:38 -0700

    comments, cleanup

--- power_shovel_docker/modules/bower/config.py
+++ power_shovel_docker/modules/bower/config.py
@@ -7,7 +7,7 @@ from power_shovel.utils.decorators import classproperty
 class BowerConfig(Config):
 
     @classproperty
-    def MODULE(cls):
+    def MODULE_DIR(cls):
         """Directory where shovel.webpack is installed"""
         from power_shovel_docker.modules import bower
         return os.path.dirname(os.path.realpath(bower.__file__))
@@ -15,7 +15,7 @@ class BowerConfig(Config):
     BUILDER_TAG = 'builder.bower'
     IMAGE_TAG = '{PROJECT_NAME}.bower'
     COMPONENTS_DIR = '{DOCKER.APP_DIR}/bower_components'
-    BUILDER_CONTEXT = '{BOWER.MODULE}'
+    BUILDER_CONTEXT = '{BOWER.MODULE_DIR}'
     BUILDER_DOCKERFILE = '{BOWER.BUILDER_CONTEXT}/Dockerfile'
     CONFIG_FILE = 'bower.json'
     CONFIG_FILE_PATH = '{DOCKER.PROJECT_DIR}/{BOWER.CONFIG_FILE}'
--- power_shovel_docker/modules/bower/tasks.py
+++ power_shovel_docker/modules/bower/tasks.py
@@ -1,23 +1,37 @@
 from power_shovel import task
+from power_shovel.modules.filesystem.file_hash import FileHash
+from power_shovel_docker.modules.docker.checker import DockerImageExists
 from power_shovel_docker.modules.docker.utils import build_image
+from power_shovel_docker.modules.docker.utils import build_library_volumes
 from power_shovel_docker.modules.docker.utils import run_builder
-from power_shovel_docker.modules.docker.utils import build_volume_from_image
 from power_shovel_docker.modules.docker.utils import build_library_image
 from power_shovel_docker.modules.npm.utils import npm_local_package_mount_flags
 from power_shovel.config import CONFIG
 
 
-@task()
+@task(
+    check=[
+        FileHash('{BOWER.MODULE_DIR}'),
+        DockerImageExists('{BOWER.BUILDER_TAG}')
+    ],
+)
 def build_bower_builder():
+    """Builder image for building bower library."""
     build_image(CONFIG.BOWER.BUILDER_TAG,
                 CONFIG.BOWER.BUILDER_DOCKERFILE,
                 context=CONFIG.BOWER.BUILDER_CONTEXT)
 
 
 def bower_builder_kwargs(image=CONFIG.BOWER.BUILDER_TAG):
+    """Build kwargs needed when using the bower builder.
+
+    :param image: tag of builder image to use.
+    :return: dict of kwarg to pass to `compose`
+    """
     volumes = [
         '{PWD}:{DOCKER.PROJECT_DIR}',
-        # TODO : volume names shouldn't be defined here
+
+        # bash_history isn't working, might need to generate empty file first
         #'{BUILDER}/bower.bash_history:{DOCKER.HOME_DIR}/.bash_history'
     ]
 
@@ -39,19 +53,22 @@ def bower_builder_kwargs(image=CONFIG.BOWER.BUILDER_TAG):
     )
 
 
-@task()
+@task(depends=[build_bower_builder],
+      parent='build_docker_images')
 def build_bower_image(tag='bower', image=CONFIG.BOWER.BUILDER_TAG):
+    """Build bower library image."""
     build_library_image(tag, **bower_builder_kwargs(image))
 
 
-@task()
-def build_bower_volume(tag='bower'):
-    build_bower_image(tag)
-    build_volume_from_image(
-        'bower', CONFIG.BOWER.MODULES_DIR, 'bower')
+@task(check=FileHash('bower.json'),
+      depends=[build_bower_builder],
+      parent='build_docker_volumes')
+def build_bower_volume():
+    """Build bower library volume."""
+    build_library_volumes(**bower_builder_kwargs())
 
 
-@task()
+@task(depends=[build_bower_builder])
 def bower_builder_shell(image=CONFIG.BOWER.BUILDER_TAG):
     """
     open a bash shell in the bower builder with volumes mounted. This allows
