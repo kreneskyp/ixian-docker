53c850190b7b3085e336a96702593ae3e5b27787
Peter Krenesky <pkrenesky@counsyl.com>
2018-07-04 14:57:09 -0700

    unused config

--- power_shovel_docker/modules/webpack/__init__.py
+++ power_shovel_docker/modules/webpack/__init__.py
@@ -3,9 +3,7 @@ MODULE_CONFIG = {
     'tasks': 'power_shovel_docker.modules.webpack.tasks',
     'config': 'power_shovel_docker.modules.webpack.config.WebpackConfig',
     'dockerfile_template': '{WEBPACK.MODULE_DIR}/Dockerfile.template',
-    'docker_context': '{WEBPACK.DOCKER_CONTEXT}',
     'dev_volumes': [
         '{WEBPACK.COMPILED_STATIC_VOLUME}:{WEBPACK.COMPILED_STATIC_DIR}',
-        '{WEBPACK.DOCKER_CONTEXT}/webpack.sh:{DOCKER.APP_DIR}/webpack.sh'
     ]
 }
