68b500ddad76f1c06293bd69d5b9159c94e122ab
Peter Krenesky <pkrenesky@counsyl.com>
2018-07-04 11:34:04 -0700

    docs/cleanup

--- power_shovel_docker/modules/npm/config.py
+++ power_shovel_docker/modules/npm/config.py
@@ -12,8 +12,15 @@ class NPMConfig(Config):
         from power_shovel_docker.modules import npm
         return os.path.dirname(os.path.realpath(npm.__file__))
 
-    NODE_MODULES_DIR = '{DOCKER.APP_DIR}/node_modules'
+    # file name for NPM config
     PACKAGE_JSON = 'package.json'
-    DOCKER_CONTEXT = '{NPM.MODULE_DIR}/context'
-    DOCKERFILE_TEMPLATE = '{NPM.MODULE_DIR}/Dockerfile.template'
+
+    # Directory in container where node_modules is located.
+    NODE_MODULES_DIR = '{DOCKER.APP_DIR}/node_modules'
+
+    # Tag for docker volume containing node_modules.
     NODE_MODULES_VOLUME = '{PROJECT_NAME}.node_modules'
+
+    # Dockerfile template for NPM support. This snippet is rendered with the
+    # base dockerfile template to create a single combined docker image.
+    DOCKERFILE_TEMPLATE = '{NPM.MODULE_DIR}/Dockerfile.template'
