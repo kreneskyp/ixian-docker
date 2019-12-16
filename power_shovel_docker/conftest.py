9eca164d6027977395a6f0a9263ec135f2bcb84d
Peter Krenesky <pkrenesky@counsyl.com>
2019-12-15 18:39:43 -0800

    test_image fixture for testing with docker image

--- power_shovel_docker/conftest.py
+++ power_shovel_docker/conftest.py
@@ -1,1 +1,16 @@
+import pytest
 from power_shovel.conftest import *
+from power_shovel_docker.modules.docker.utils.images import build_image
+from power_shovel_docker.tests.mocks.client import *
+
+
+TEST_IMAGE_NAME = "power_shovel_docker.scratch"
+
+
+@pytest.fixture
+def test_image(mock_docker_environment):
+    build_image(
+        TEST_IMAGE_NAME,
+        file="/opt/power_shovel_docker/power_shovel_docker/tests/Dockerfile.scratch",
+    )
+    yield
