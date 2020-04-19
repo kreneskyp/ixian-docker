8c8205f13b7409f442a9b462968ac3ae007b4218
Peter Krenesky <pkrenesky@counsyl.com>
2020-04-19 13:35:38 -0700

    add pytest module

---
+++ power_shovel_docker/modules/pytest/__init__.py
@@ -0,0 +1,5 @@
+OPTIONS = {
+    "name": "PYTEST",
+    "tasks": "power_shovel_docker.modules.pytest.tasks",
+    "config": "power_shovel_docker.modules.pytest.config.PytestConfig",
+}
---
+++ power_shovel_docker/modules/pytest/config.py
@@ -0,0 +1,15 @@
+from power_shovel.config import Config
+
+
+class PytestConfig(Config):
+    SRC = "{PYTHON.ROOT_MODULE_PATH}"
+    CACHE_DIR = "{DOCKER.APP_DIR}/.pytest_cache"
+    INI_FILE = "{DOCKER.APP_ETC}/runtime/pytest.ini"
+    ARGS = [
+        "-c {PYTEST.INI_FILE}",
+        "-o cache_dir={PYTEST.CACHE_DIR}",
+    ]
+    SPLIT=3
+
+
+PYTEST_CONFIG = PytestConfig()
